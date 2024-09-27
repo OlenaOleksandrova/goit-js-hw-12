6977beb4a198f8172c8d5dd1299abd7fd46fea08
Olena Oleksandrova <ha240886kev@gmail.com>
2024-09-28 02:09:26 +0300

    js

--- src/main.js
+++ src/main.js
@@ -6,10 +6,13 @@ const searchForm = document.querySelector('.search-form');
 // const loaderEl = document.querySelector('.loader'); 
 const imagesBoxEl = document.querySelector('.gallery');
 const LoadMore = document.querySelector('.js-load-more')
+let currentPage = 1;
+
 
 // подія форми
 
 searchForm.addEventListener('submit', handleSubmit);
+// LoadMore.addEventListener('click', handleLoadMore);
    
 
 async function handleSubmit(event) {
@@ -31,7 +34,13 @@ async function handleSubmit(event) {
         const imagesMarkup = renderImages(response.hits);
         imagesBoxEl.innerHTML = imagesMarkup;
 
-        LoadMore.classList.remove('is-hidden'); 
+          if (response.totalHits > currentPage * 6) {
+            LoadMore.classList.remove('is-hidden'); // Показуємо кнопку
+        } else {
+            LoadMore.classList.add('is-hidden'); // Ховаємо кнопку
+        }
+
+        // LoadMore.classList.remove('is-hidden'); 
         
     } catch (error) {
         console.error('Error:', error);
