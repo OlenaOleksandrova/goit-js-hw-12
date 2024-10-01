bbb54eb4df12706745d1f37ca3a4c17bc3a0d1e6
Olena Oleksandrova <ha240886kev@gmail.com>
2024-10-02 00:05:15 +0300

    js

--- src/main.js
+++ src/main.js
@@ -9,114 +9,119 @@ const searchForm = document.querySelector('.search-form');
 const imagesBoxEl = document.querySelector('.gallery');
 const loadMore = document.querySelector('.js-load-more');
 const messageBox = document.querySelector('.message');
+const loader = document.querySelector('.loader'); 
 
 let currentPage = 1;
 let query = null;
-let pages = 0;
-let gallery;
+let totalPages = 0;
+let gallery = null;
 
 // подія форми
 
 searchForm.addEventListener('submit', handleSubmit);
 loadMore.addEventListener('click', handleLoadMore);
-   
-// let gallery = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });  
 
 async function handleSubmit(event) {
     event.preventDefault();
 
-     const form = event.currentTarget;
+    // loader.classList.remove('is-hidden');
+    const form = event.currentTarget;
     query = form.elements.query.value.trim(); 
  
     if (!query) {
         return;
     }
 
-     currentPage = 1;
+    currentPage = 1;
+    imagesBoxEl.innerHTML = ''; 
+    loadMore.classList.add('is-hidden');
 
     try {
+        showLoader(); 
         const response = await fetchImages(query, currentPage);
-        pages = Math.ceil(response.totalHits / PER_PAGE);
-        console.log('handleSubmit:', response);
+        totalPages = Math.ceil(response.totalHits / PER_PAGE);
 
          if (response.totalHits === 0) {
             showMessage("Sorry, there are no images matching your search query. Please try again!");
-            imagesBoxEl.innerHTML = '';
-            loadMore.classList.add('is-hidden');
+            // loadMore.classList.add('is-hidden');
             return;
         }
 
         const imagesMarkup = renderImages(response.hits);
         imagesBoxEl.innerHTML = imagesMarkup;
 
-         gallery = new SimpleLightbox('.gallery a', {
-            captionsData: 'alt',
-            captionDelay: 250,
-        }); 
-
-        // gallery.refresh();
-
-         if (response.totalHits > currentPage * PER_PAGE) {
+          if (!gallery) {
+            gallery = new SimpleLightbox('.gallery a', {
+                captionsData: 'alt',
+                captionDelay: 250,
+            });
+        } else {
+            gallery.refresh();
+        }
+       
+        if (currentPage < totalPages) {  
              loadMore.classList.remove('is-hidden'); // Показ. кнопку
-         } else {
-             loadMore.classList.add('is-hidden'); // Хов. кнопку
-}
-
-        // LoadMore.classList.remove('is-hidden'); 
-        
-    } catch (error) {
-        console.error('Error:', error);
-
+        }
+      } catch (error) {
+          console.error('Error:', error);
     } finally {
+        hideLoader(); 
         form.reset();
     }
 }
 
 async function handleLoadMore() {
     currentPage += 1;
-    console.log(pages);
     try {
-       
+        showLoader(); 
          const response = await fetchImages(query, currentPage);
-        console.log('handleLoadMore:', response);
+        // console.log('handleLoadMore:', response);
 
-         if (response.totalHits === 0) {
-            showMessage("Sorry, there are no images matching your search query. Please try again!");
-            return;
-        }
-
-const imagesMarkup = renderImages(response.hits);
+         const imagesMarkup = renderImages(response.hits);
         imagesBoxEl.insertAdjacentHTML('beforeend', imagesMarkup);
-        // imagesBoxEl.innerHTML = imagesMarkup;
 
-          gallery.refresh();
+        //  if (response.totalHits === 0) {
+        //     showMessage("Sorry, there are no images matching your search query. Please try again!");
+        //     return;
+        // }
+        gallery.refresh();
 
-        handleScrollView();
-        
-        if (currentPage >= pages) {
+        if (currentPage >= totalPages) {
             loadMore.classList.add('is-hidden');
-            console.log("We're sorry, but you've reached the end of search results.");
-        }
+            showMessage("We're sorry, but you've reached the end of search results.");
+        } 
+        
+        handleScrollView(); 
+     
     } catch (error) {
          console.error('Error:', error);
-     }
+     } finally {
+         hideLoader();
+    }
+}
+
+// / Показ. завантажувач
+function showLoader() {
+    loader.style.display = 'block';
+}
+
+// Хов. завантажувач
+function hideLoader() {
+    loader.style.display = 'none';
 }
 
 // показати повідомлення при 0 результаті
 function showMessage(message) {
     messageBox.textContent = message;
-    messageBox.classList.remove('is-hidden'); // Показ. повідомлення
+    messageBox.classList.remove('is-hidden');
 
     // Сховати повідомлення
     setTimeout(() => {
         messageBox.classList.add('is-hidden'); 
-        messageBox.textContent = ''; 
+        // messageBox.textContent = ''; 
     }, 3000);
 }
- 
+
 // Скрол після завантаження партії картинок
 function handleScrollView() {
     const lastArticle = imagesBoxEl.lastElementChild;
